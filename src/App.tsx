a9bdc6f9fac5e8eeb297ca978a5409677a93bf70
Karanveer Singh <karanveersingh7@rrc.ca>
2025-09-16 12:44:10 -0500

    updating file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import Header from "./components/Header"
-import NavBar from "./components/NavBar"
-import Footer from "./components/Footer"
-import Music from "./components/Music"
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Music from './pages/Music';
 
-
-function App() {
+export default function App() {
   return (
-    <div>
-      <Header />
-      <NavBar />
-      <main>
-      <Music />
-      </main>
-      <Footer />
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/music" element={<Music />} />
+    </Routes>
+  );
 }
-
-export default App
