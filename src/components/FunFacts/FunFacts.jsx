5cfca7ce21b78048b25e8f0b32f030c485f90c20
Aakash <aaakash@academic.rrc.ca>
2025-09-20 08:54:40 -0500

    Render categories dynamically with Object.entries loop

--- src/components/FunFacts/FunFacts.jsx
+++ src/components/FunFacts/FunFacts.jsx
@@ -4,6 +4,14 @@ function FunFacts() {
   return (
     <section className="funfacts" id="funfacts">
       <h1>Fun Facts</h1>
+
+      {Object.entries(funfacts).map(([category, videos]) => (
+        <article key={category}>
+          <h2>{category}</h2>
+          <div className="grid">
+          </div>
+        </article>
+      ))}
     </section>
   );
 }
