303ef211c7719f3e56ffa772dba3510e41e5be49
Aakash <aaakash@academic.rrc.ca>
2025-09-20 08:57:27 -0500

    Add reusable VideoCard sub-component

---
+++ src/components/FunFacts/VideoCard.jsx
@@ -0,0 +1,17 @@
+function VideoCard({ video }) {
+  return (
+    <div className="card">
+      <iframe
+        src={video.url}
+        title={video.title}
+        loading="lazy"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      <h3>{video.title}</h3>
+      <p>{video.description}</p>
+    </div>
+  );
+}
+
+export default VideoCard;
