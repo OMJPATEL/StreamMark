5b36f47d628fd0f5ab599d1585fe54a9a139bccd
Aakash <aaakash@academic.rrc.ca>
2025-09-20 08:50:29 -0500

    Create FunFacts component skeleton with heading

---
+++ src/components/FunFacts/FunFacts.jsx
@@ -0,0 +1,11 @@
+import funfacts from "../../data/funfacts.json"
+
+function FunFacts() {
+  return (
+    <section className="funfacts" id="funfacts">
+      <h1>Fun Facts</h1>
+    </section>
+  );
+}
+
+export default FunFacts;
