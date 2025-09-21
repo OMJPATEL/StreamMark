6a35cea08d509dd641b34d94b47b2bfb3ad69301
Aakash <aaakash@academic.rrc.ca>
2025-09-21 18:33:19 -0500

    Final commit.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Team Members: Karanveer | Aakash | Akash Kumar | Om Patel</p>
+      <p>&copy; {new Date().getFullYear()} StreamMark</p>
+    </footer>
+  )
+}
+export default Footer
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li><a href="#education">Educational</a></li>
+        <li><a href="#funfacts">Fun-Facts</a></li>
+        <li><a href="#movies">Movies</a></li>
+        <li><a href="#music">Music</a></li>
+      </ul>
+    </nav>
+  )
+}
+export default NavBar
