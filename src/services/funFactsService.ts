6eb9360489039fd14bbbeb62696aab7ae5d000b3
Aakash <aaakash@academic.rrc.ca>
2025-11-17 15:39:51 -0600

    refactor: simplify FunFacts service to use async backend repository.

--- src/services/funFactsService.ts
+++ src/services/funFactsService.ts
@@ -1,14 +1,7 @@
 import { funFactsRepository } from "../repositories/funFactsRepository";
-import type { FunFact } from "../repositories/funFactsRepository";
-
-type FunFactData = Record<string, FunFact[]>;
 
 export const funFactsService = {
-  getAll(): FunFactData {
+  getAll: async () => {
     return funFactsRepository.getAll();
-  },
-
-  getByCategory(category: string): FunFact[] {
-    return funFactsRepository.getByCategory(category);
-  },
+  }
 };
